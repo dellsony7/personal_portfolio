059c0f712168b3327a66794618d7fc8031801596
Dellsony Dissanayaka <eng.dellsony@gmail.com>
2023-06-07 11:42:40 +0530

    cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -171,7 +171,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1yfHkMuYXc_jgismYY_apXRP5rYPSnWtm/view?usp=sharing"
+              href="https://drive.google.com/file/d/1a_WTpEhjV2oypE9C-TSRZ017aFAVIaNm/view?usp=sharing"
+              // href="https://drive.google.com/file/d/1yfHkMuYXc_jgismYY_apXRP5rYPSnWtm/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
