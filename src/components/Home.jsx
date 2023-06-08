9905986777077b17910aa3b826efa2050531d3fa
Dellsony Dissanayaka <eng.dellsony@gmail.com>
2023-06-09 02:22:06 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -69,7 +69,7 @@ const Home = () => {
       <section id="home" className="bg-[#010201]">
         <div className=" w-full h-screen container mx-auto flex px-10 py-20 md:flex-row flex-col items-center bg-[#010201] text-gray-300">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="text-4xl sm:text-7xl font-bold text-[#ffffff]">
+            <h1 className=" py-10 text-4xl sm:text-7xl font-bold text-[#ffffff]">
               DELSONY DISSANAYAKA
             </h1>
             <h2 className="text-4xl sm:text-7xl font-bold text-[#999999]">
