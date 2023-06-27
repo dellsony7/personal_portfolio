1da5e2c55c8c42cf5b54632207a1c1dad74c42f1
Dellsony Dissanayaka <eng.dellsony@gmail.com>
2023-06-28 00:23:13 +0530

    edit 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -184,16 +184,16 @@ const Navbar = () => {
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+          {/* <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1a_WTpEhjV2oypE9C-TSRZ017aFAVIaNm/view?usp=sharing"
+              href=""
               target="_blank"
               rel="noopener noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
