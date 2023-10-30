70d5737f1abd4ca836467bb1c7ee71a5fa4929b5
dellsony7 <techgen.srilanka@gmail.com>
2023-10-31 01:23:37 +0530

    Skill Updates

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,6 +13,8 @@ import Bitbucket from "../assets/bitbucket.png";
 import Illuustrator from "../assets/illustrator.png";
 import Photoshop from "../assets/photoshop.png";
 import C from "../assets/c-logo-vector.png";
+import Mongo from "../assets/mongo.png";
+import Express from "../assets/express.png";
 
 const Skills = () => {
   return (
@@ -29,12 +31,25 @@ const Skills = () => {
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
-            <p className="my-4">JAVASCRIPT</p>
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <p className="my-4">MongoDB</p>
+          </div>
+          <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Express} alt="HTML icon" />
+            <p className="my-4">ExpressJS</p>
           </div>
           <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p className="my-4">REACT</p>
+            <p className="my-4">React</p>
           </div>
+          <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <p className="my-4">NodeJS</p>
+          </div>
+
           <div className="shadow-md shadow-green-300 hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
@@ -77,8 +92,6 @@ const Skills = () => {
             <img className="w-20 mx-auto" src={Photoshop} alt="HTML icon" />
             <p className="my-4">Photoshop</p>
           </div> */}
-
-
         </div>
       </div>
     </div>
