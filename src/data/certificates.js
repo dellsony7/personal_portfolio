d2ed07de701ae062f95cc29c53b9ec5d971fc9c6
dellsony7 <eng.dellsony@gmail.com>
2024-03-14 00:22:14 +0530

    certification added

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -3,7 +3,10 @@ import jsintermeidate from "../assets/Certificates/JSintermediate.png"
 import jsintro from "../assets/Certificates/jsintroduction.png"
 import pythonintro from "../assets/Certificates/introductiontopython.png"
 import responsive_web from "../assets/Certificates/responsiveWeb.png"
-import  frontenddevlibraries  from "../assets/Certificates/frontenddevlibraries.png";
+import frontenddevlibraries from "../assets/Certificates/frontenddevlibraries.png"
+import angular from "../assets/Certificates/angular_certificate.png"
+import html from "../assets/Certificates/html.png"
+import web_dev from "../assets/Certificates/web_dev.png"
 
 export const data = [
   {
@@ -17,33 +20,51 @@ export const data = [
     image: jsintermeidate,
     github: "https://www.sololearn.com/certificates/CC-F3K9WVUG",
   },
-  {
-    id: 3,
-    name: "Intorduction to JavaScript",
-    image: jsintro,
-    github: "https://www.sololearn.com/certificates/CC-JBLHJ5SA",
-  },
-  {
-    id: 4,
-    name: "Introduction to Python",
-    image: pythonintro,
-    github: "https://www.sololearn.com/certificates/CC-CVVGLOXP",
-  },
-  {
-    id: 5,
-    name: "Introduction to Python",
-    image: responsive_web,
-    github:
-      "https://www.freecodecamp.org/certification/fcc6196b037-a1a1-4835-82f6-52ac4c252b4b/responsive-web-design",
-  },
 
   {
-    id: 5,
+    id: 3,
     name: "Front End Development Libraries",
     image: frontenddevlibraries,
     github:
       "https://www.freecodecamp.org/certification/fcc6196b037-a1a1-4835-82f6-52ac4c252b4b/front-end-development-libraries",
   },
+  {
+    id: 4,
+    name: "Introduction to Python",
+    image: responsive_web,
+    github:
+      "https://www.freecodecamp.org/certification/fcc6196b037-a1a1-4835-82f6-52ac4c252b4b/responsive-web-design",
+  },
+  {
+    id: 9,
+    name: "Introduction to Python",
+    image: web_dev,
+    github: " https://www.sololearn.com/certificates/CC-OTXDI04D",
+  },
+
+  {
+    id: 8,
+    name: "Introduction to HTML",
+    image: html,
+    github: "  https://www.sololearn.com/certificates/CC-S3JVEMHX",
+  },
+
+  {
+    id: 5,
+    name: "Angular",
+    image: angular,
+    github: "https://www.sololearn.com/certificates/CC-LL8FLBL6s",
+  },
+  {
+    id: 6,
+    name: "Intorduction to JavaScript",
+    image: jsintro,
+    github: "https://www.sololearn.com/certificates/CC-JBLHJ5SA",
+  },
+  {
+    id: 7,
+    name: "Introduction to Python",
+    image: pythonintro,
+    github: "https://www.sololearn.com/certificates/CC-CVVGLOXP",
+  },
 ]
-
-
